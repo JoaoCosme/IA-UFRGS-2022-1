32342b26d08e80d26a30459e8f1698b16d466cfe
JoaoCosme <cosmejoaopedro@protonmail.com>
2022-07-09 17:31:50 -0300

    Refatora metodo de calculo de distancia

--- Trab-1/manhattan.py
+++ Trab-1/manhattan.py
@@ -1,25 +1,23 @@
 from solucao import Nodo, ESTADO_FINAL, POSICAO_VAZIA,NUMERO_COLUNAS,NUMERO_LINHAS
 
-
-
-
 def calcula_distancia_posicao(estado_atual:str,numero:str)->int:
     posicao_correta = ESTADO_FINAL.find(numero)
     posicao_atual = estado_atual.find(numero)
     
     
-    calcula_dist_vertical = lambda posicao_correta, posicao_atual: abs(posicao_correta//NUMERO_LINHAS-posicao_atual//NUMERO_LINHAS)
+    calcula_distancia_vertical = lambda posicao_correta, posicao_atual: abs(posicao_correta//NUMERO_LINHAS-posicao_atual//NUMERO_LINHAS)
+    calcula_distancia_horizontal = lambda posicao_correta, posicao_atual:abs(posicao_correta%NUMERO_COLUNAS-posicao_atual%NUMERO_COLUNAS)
     
-    na_coluna_correta = lambda posicao_correta,posicao_atual:posicao_correta%NUMERO_COLUNAS ==posicao_atual%NUMERO_COLUNAS
-    na_linha_correta = lambda posicao_correta,posicao_atual:calcula_dist_vertical(posicao_correta,posicao_atual)<2
+    na_coluna_correta = lambda posicao_correta,posicao_atual:calcula_distancia_vertical(posicao_correta,posicao_atual)==0
+    na_linha_correta = lambda posicao_correta,posicao_atual:abs(posicao_correta-posicao_atual)<2
     
     if na_linha_correta(posicao_correta,posicao_atual) :
-        return abs(posicao_correta-posicao_atual)
+        return calcula_distancia_horizontal(posicao_atual,posicao_correta)
     elif na_coluna_correta(posicao_correta,posicao_atual):
-        return abs(posicao_correta//3-posicao_atual//3)    
+        return calcula_distancia_vertical(posicao_atual,posicao_correta)   
     else:
-        distancia_horizontal = abs(posicao_correta%3-posicao_atual%3)
-        distancia_vertical = calcula_dist_vertical(posicao_atual,posicao_correta)
+        distancia_horizontal = calcula_distancia_horizontal(posicao_atual,posicao_correta)
+        distancia_vertical = calcula_distancia_vertical(posicao_atual,posicao_correta)
         return distancia_horizontal+distancia_vertical
     
 
