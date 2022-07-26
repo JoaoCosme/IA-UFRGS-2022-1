ebb37e7e14b483205f0bf475e150bbb627e9a0e3
JoaoCosme <cosmejoaopedro@protonmail.com>
2022-07-26 17:36:24 -0300

    Adiciona kit trab 2

---
+++ Geral/ucv.py
@@ -0,0 +1,11 @@
+from math import log, sqrt
+
+
+def ucb(vitoria_suc,jogadas_suc,jogadas_no):
+    return vitoria_suc/jogadas_suc + 1.1 * sqrt(2*log(jogadas_no)/jogadas_suc)
+
+
+while True:
+    enter = input("Enter:").split(",")
+    enter = [ int(x1) for x1 in enter]
+    print(ucb(enter[0],enter[1],enter[2]))
