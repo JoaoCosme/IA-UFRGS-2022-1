d8ecd5a9e69b7e542f23fc07eb226cd77749b3e6
JoaoCosme <cosmejoaopedro@protonmail.com>
2022-08-09 20:59:09 -0300

    Format

--- Trab-2/advsearch/your_agent/aval_functs/avalicao.py
+++ Trab-2/advsearch/your_agent/aval_functs/avalicao.py
@@ -2,14 +2,15 @@ from typing import List
 from advsearch.othello.board import Board
 import advsearch.your_agent.aval_functs.funcoes_avaliacao as funcoes_avaliacao
 
-PESO_QUANTIDADE_PECAS = 1
-avaliacoes:List = [(funcoes_avaliacao.avaliacao_quantidade_de_pecas,PESO_QUANTIDADE_PECAS)]
+PESO_QUANTIDADE_PECAS = 2
+PESO_ESPACOS_VAZIOS = 1
+avaliacoes: List = [(funcoes_avaliacao.avaliacao_quantidade_de_pecas, PESO_QUANTIDADE_PECAS),
+                    (funcoes_avaliacao.avaliacao_menos_espacos_vazios, PESO_ESPACOS_VAZIOS)]
 
 
-
-def avalia(board:Board,cor):
+def avalia(board: Board, cor):
     pontuacao_total = 0
     for avaliacao in avaliacoes:
-        resultado_avaliacao = avaliacao[0](board,cor)
+        resultado_avaliacao = avaliacao[0](board, cor)
         pontuacao_total += resultado_avaliacao*avaliacao[1]
     return pontuacao_total
--- Trab-2/advsearch/your_agent/aval_functs/funcoes_avaliacao.py
+++ Trab-2/advsearch/your_agent/aval_functs/funcoes_avaliacao.py
@@ -1,4 +1,5 @@
 
+from tkinter.font import BOLD
 from advsearch.othello.board import Board
 
 inverte_cor = lambda cor: Board.WHITE if cor == Board.BLACK else Board.BLACK
@@ -6,3 +7,5 @@ inverte_cor = lambda cor: Board.WHITE if cor == Board.BLACK else Board.BLACK
 def avaliacao_quantidade_de_pecas(board:Board,cor):
     return board.num_pieces(cor) - board.num_pieces(inverte_cor(cor))
     
+def avaliacao_menos_espacos_vazios(board:Board,cor):
+    return board.num_pieces(Board.EMPTY)
