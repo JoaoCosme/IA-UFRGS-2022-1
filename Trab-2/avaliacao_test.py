ecb501fc53983889c65c7a0d64c0c05789e3b0cd
JoaoCosme <cosmejoaopedro@protonmail.com>
2022-08-09 21:00:39 -0300

    Format documet

--- Trab-2/avaliacao_test.py
+++ Trab-2/avaliacao_test.py
@@ -6,15 +6,18 @@ from advsearch.othello.board import Board
 
 class SimpleTest(unittest.TestCase):
 
-	def test(self):		
-		self.assertEqual(1,avaliacao.avalia(criaBoard(1,0),Board.BLACK),"Avalia corretamente para numero de pecas")
+    def test(self):
+        self.assertEqual(3, avaliacao.avalia(
+            criaBoard(1, 0, 1), Board.BLACK), "Avalia corretamente para numero de pecas")
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
 
 
-def criaBoard(numPretas:int,numBrancas:int):
+def criaBoard(numPretas: int, numBrancas: int, numVazias: int):
     board = Board()
     board.piece_count[Board.BLACK] = numPretas
     board.piece_count[Board.WHITE] = numBrancas
+    board.piece_count[Board.EMPTY] = numVazias
     return board
