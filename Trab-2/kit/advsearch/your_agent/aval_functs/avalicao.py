639c8836fad37d7f2b284f5618556b6e919d4f93
JoaoCosme <cosmejoaopedro@protonmail.com>
2022-08-11 19:52:29 -0300

    atualiza pesos

--- Trab-2/kit/advsearch/your_agent/aval_functs/avalicao.py
+++ Trab-2/kit/advsearch/your_agent/aval_functs/avalicao.py
@@ -3,14 +3,15 @@ from advsearch.othello.board import Board
 import advsearch.your_agent.aval_functs.funcoes_avaliacao as funcoes_avaliacao
 
 #Conjunto de tuplas avaliacao/peso
-avaliacoes: List = [(funcoes_avaliacao.avaliacao_quantidade_de_pecas, 2),
+avaliacoes: List = [(funcoes_avaliacao.avaliacao_quantidade_de_pecas, 5),
                     (funcoes_avaliacao.avaliacao_menos_espacos_vazios, 1),
                     (funcoes_avaliacao.avaliacao_diferenca_pecas, 2),
                     (funcoes_avaliacao.avaliacao_menos_ataques_oponente, 1),
                     (funcoes_avaliacao.avaliacao_mais_ataques_jogador, 2),
                     (funcoes_avaliacao.avaliacao_e_vitoria, 100),
                     (funcoes_avaliacao.avaliacao_mobilidade,10),
-                    (funcoes_avaliacao.avalia_cantos_capturados,20)]
+                    (funcoes_avaliacao.avalia_cantos_capturados,40),
+                    (funcoes_avaliacao.avalia_estabilidade,20)]
                     
 
 def avalia(board: Board, cor):
