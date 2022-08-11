f3b3ae9e66eb1f757386b94d6dd894be7d7c7d7d
JoaoCosme <cosmejoaopedro@protonmail.com>
2022-08-11 19:37:52 -0300

    add funcao de avaliar cantos capturados

--- Trab-2/kit/advsearch/your_agent/aval_functs/funcoes_avaliacao.py
+++ Trab-2/kit/advsearch/your_agent/aval_functs/funcoes_avaliacao.py
@@ -1,4 +1,7 @@
+from calendar import c
 from advsearch.othello.board import Board
+
+coordenadas_cantos = [(0,0),(0,8),(8,0),(8,8)]
 
 inverte_cor = lambda cor: Board.WHITE if cor == Board.BLACK else Board.BLACK
 
@@ -19,4 +22,12 @@ def avaliacao_mais_ataques_jogador(board:Board,cor):
 
 def avaliacao_e_vitoria(board:Board,cor):
     return 100 if board.winner() == cor and board.is_terminal_state() else 0;
-    
+
+def avaliacao_mobilidade(board:Board,cor):
+    return 100 * (board.legal_moves(cor)-board.legal_moves(inverte_cor(cor))) / (board.legal_moves(cor)+board.legal_moves(inverte_cor(cor)))
+
+def avalia_cantos_capturados(board:Board,cor):
+    cantos_capturados = 0
+    for canto in coordenadas_cantos:
+        cantos_capturados += 1 if board.tiles[canto[0]][canto[1]]==cor else 0
+    return cantos_capturados * 100 / 4
