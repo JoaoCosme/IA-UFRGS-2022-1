ca4d3b5713ac6fca0023819ac1cee5e9dc17554a
asvarella <alice.varella@gmail.com>
2022-08-12 10:52:11 -0300

    adicionando min max e ajustando nomes

--- Trab-2/kit/advsearch/alfa_beta_player/agent.py
+++ Trab-2/kit/advsearch/alfa_beta_player/agent.py
@@ -3,6 +3,7 @@ import random
 import sys
 from advsearch.your_agent.aval_functs.avalicao import avalia
 from advsearch.othello.board import Board
+import pickle # biblioteca para fazer a cópia do estado do tabuleiro
 
 
 # Voce pode criar funcoes auxiliares neste arquivo
@@ -11,26 +12,15 @@ from advsearch.othello.board import Board
 # Nao esqueca de renomear 'your_agent' com o nome
 # do seu agente.
 
-# constante para condição de parada da poda alfa beta
-PROFUNDIDADE_MAXIMA = 6
-
-
-def func_avalicao(stado, color):
-    return None
-
-
-def poda_alfa_beta():
-    '''
-    if condicao_parada():
-        return avalia()
-    '''
-    return None
+NULL_MOVE = (-1,-1) # variavel para representar um movimento nulo
+DEPTH_START = 0 # inicializa a profundidade com 0
+DEPTH_LIM = 6 # representa a profundidade máxima
+VAL_MAX =  10000 #valor máximo inicial
+VAL_MIN = -10000 #valor mínimo inicial
 
 # funçao booleana que checa se a poda alfa beta chegou à profundidade maxima.
-
-
 def condicao_parada(profundidade):
-    return profundidade > PROFUNDIDADE_MAXIMA
+    return profundidade > DEPTH_LIM
 
 
 def avalia_board(the_board: Board, color):
@@ -50,6 +40,55 @@ def avalia_board(the_board: Board, color):
 
     return melhor_pos
 
+def player_max(tabuleiro, color, alpha, beta, depth): #Função que representa o jogador MAX
+
+    if condicao_parada(depth): #Testa condição de parada 
+        return avalia(tabuleiro, color), NULL_MOVE #Caso ela retorne algo, devolve a função de avaliação com movimento nulo
+    
+    # inicialização das variaveis
+    val = VAL_MIN
+    move = NULL_MOVE
+    legal_moves = tabuleiro.legal_moves(color)
+
+    if len(legal_moves) == 0: 
+        legal_moves = [NULL_MOVE] #Se jogador atual não possui jogadas válidas, devolve movimeto vazio
+
+    # loop que descobre o melhor movimento no estado atual
+    for move_prox in legal_moves:
+        state = pickle.loads(pickle.dumps(tabuleiro))
+        state.process_move(move_prox, color)
+        temp_val, temp_move = player_min(state, tabuleiro.opponent(color), alpha, beta, depth + 1) #Define se deve atualizar ou não o novo movimento
+        val, move = (temp_val, move_prox) if temp_val > val else (val, move)
+        alpha = max(alpha, val)
+        
+        if alpha >= beta: # Para se MIN possui uma jogada melhor
+            break
+    return val, move
+
+def player_min(tabuleiro, color, alpha, beta, depth): #Função que representa o jogador MIN
+
+    if condicao_parada(depth): #Testa condição de parada 
+        return avalia(tabuleiro, color), NULL_MOVE #Caso ela retorne algo, devolve a função de avaliação com movimento nulo
+    
+    # inicialização das variaveis
+    val = VAL_MAX
+    move = NULL_MOVE
+    legal_moves = tabuleiro.legal_moves(color)
+
+    if len(legal_moves) == 0: 
+        legal_moves = [NULL_MOVE] #Se jogador atual não possui jogadas válidas, devolve movimeto vazio
+
+    # loop que descobre o melhor movimento no estado atual
+    for new_move in legal_moves:
+        state = pickle.loads(pickle.dumps(tabuleiro))
+        state.process_move(new_move, color)
+        temp_val, temp_move = player_max(state, tabuleiro.opponent(color), alpha, beta, depth + 1) #Define se deve atualizar ou não o novo movimento
+        val, move = (temp_val, new_move) if temp_val < val else (val, move)
+        beta = min(beta, val)
+        
+        if beta <= alpha: # Para se MAX possui uma jogada melhor
+            break
+    return val, move
 
 def make_move(the_board: Board, color):
     """
@@ -62,4 +101,6 @@ def make_move(the_board: Board, color):
     # o codigo abaixo apenas retorna um movimento aleatorio valido para
     # a primeira jogada com as pretas.
     # Remova-o e coloque a sua implementacao da poda alpha-beta
-    return avalia_board(the_board, color)
+    val, move = player_max(the_board, color, VAL_MIN, VAL_MAX, DEPTH_START) #começa o MINMAX chamando a função MAX para jogar
+    #return avalia_board(the_board, color)
+    return move
