805610e92a95e51695e7f42228de147e84859682
JoaoCosme <49664603+JoaoCosme@users.noreply.github.com>
2022-08-27 16:02:06 -0300

    Adiciona gradiente do MSE (#13)
    * declara metodos
    
    * passo do gradiente implementado
    
    * refatora passo gradiente

--- Trab-3/alegrete.py
+++ Trab-3/alegrete.py
@@ -1,6 +1,18 @@
-from functools import reduce
-import pdb
-import numpy as np
+COORDENADA_X = 0
+COORDENADA_Y = 1
+VALOR_CALCULADO = 2 
+
+def deriva_theta_0(erros_por_cordenada):
+    somatorio_dos_erros = 0
+    for erro_por_coordenada in erros_por_cordenada:
+        somatorio_dos_erros += erro_por_coordenada[VALOR_CALCULADO]
+    return 2*somatorio_dos_erros/len(erros_por_cordenada)
+
+def deriva_theta_1(erros_por_cordenada):
+    somatorio_dos_erros = 0
+    for erro_por_coordenada in erros_por_cordenada:
+        somatorio_dos_erros += erro_por_coordenada[VALOR_CALCULADO] * erro_por_coordenada[COORDENADA_X]
+    return 2*somatorio_dos_erros/len(erros_por_cordenada)
 
 def calcula_y(theta_0,theta_1,x):
     return theta_0 + theta_1 * x
@@ -18,6 +30,13 @@ def compute_mse(theta_0, theta_1, data):
         somatorio_dos_quadrados += pow(calcula_y(theta_0,theta_1,data_entry[0]) - data_entry[1],2)
     return somatorio_dos_quadrados/len(data)
 
+def calcula_erros_por_cordenada(theta_0,theta_1,data):
+    valor_por_cordenada = []
+    quantidade_de_dados = len(data)
+    for i in range(quantidade_de_dados):
+        estimativa_calculada = calcula_y(theta_0,theta_1,data[i][COORDENADA_X]) - data[i][COORDENADA_Y]
+        valor_por_cordenada.append([data[i][COORDENADA_X],data[i][COORDENADA_Y],estimativa_calculada])
+    return valor_por_cordenada
 
 def step_gradient(theta_0, theta_1, data, alpha):
     """
@@ -28,8 +47,8 @@ def step_gradient(theta_0, theta_1, data, alpha):
     :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
     :return: float,float - os novos valores de theta_0 e theta_1, respectivamente
     """
-    raise NotImplementedError  # substituir pelo seu codigo
-
+    erros_por_cordenada = calcula_erros_por_cordenada(theta_0,theta_1,data)
+    return theta_0 - alpha*deriva_theta_0(erros_por_cordenada),theta_1 - alpha*deriva_theta_1(erros_por_cordenada)
 
 def fit(data, theta_0, theta_1, alpha, num_iterations):
     """
--- Trab-3/test_alegrete.py
+++ Trab-3/test_alegrete.py
@@ -14,19 +14,19 @@ class TestAlegrete(unittest.TestCase):
         mse = alegrete.compute_mse(0, 0, data)
         self.assertAlmostEqual(66.78348986604624, mse, 8)  # comparacao de floats com 9 casas de precisao
 
-    # def test_step_gradient(self):
-    #     # dataset do Quiz de Otimizacao Continua
-    #     data = np.array([
-    #         [1, 3],
-    #         [2, 4],
-    #         [3, 4],
-    #         [4, 2]
-    #     ])
+    def test_step_gradient(self):
+        # dataset do Quiz de Otimizacao Continua
+        data = np.array([
+            [1, 3],
+            [2, 4],
+            [3, 4],
+            [4, 2]
+        ])
 
-    #     new_theta0, new_theta1 = alegrete.step_gradient(1, 1, data, alpha=0.1)
-    #     # comparacao de floats com precisao de 11 casas
-    #     self.assertAlmostEqual(0.95, new_theta0, 11)
-    #     self.assertAlmostEqual(0.55, new_theta1, 11)
+        new_theta0, new_theta1 = alegrete.step_gradient(1, 1, data, alpha=0.1)
+        # comparacao de floats com precisao de 11 casas
+        self.assertAlmostEqual(0.95, new_theta0, 11)
+        self.assertAlmostEqual(0.55, new_theta1, 11)
 
 
 if __name__ == '__main__':
