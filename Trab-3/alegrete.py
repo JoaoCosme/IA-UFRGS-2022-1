e16b399ddb739cae0eb8964d767a77446f8a522a
JoaoCosme <cosmejoaopedro@protonmail.com>
2022-08-28 12:48:36 -0300

    adiciona normalizacao teste

--- Trab-3/alegrete.py
+++ Trab-3/alegrete.py
@@ -5,7 +5,8 @@ from numpy import Infinity
 
 COORDENADA_X = 0
 COORDENADA_Y = 1
-VALOR_CALCULADO = 2 
+VALOR_CALCULADO = 2
+
 
 def deriva_theta_0(erros_por_cordenada):
     somatorio_dos_erros = 0
@@ -13,14 +14,18 @@ def deriva_theta_0(erros_por_cordenada):
         somatorio_dos_erros += erro_por_coordenada[VALOR_CALCULADO]
     return 2.0*somatorio_dos_erros/len(erros_por_cordenada)
 
+
 def deriva_theta_1(erros_por_cordenada):
     somatorio_dos_erros = 0
     for erro_por_coordenada in erros_por_cordenada:
-        somatorio_dos_erros += erro_por_coordenada[VALOR_CALCULADO] * erro_por_coordenada[COORDENADA_X]
+        somatorio_dos_erros += erro_por_coordenada[VALOR_CALCULADO] * \
+            erro_por_coordenada[COORDENADA_X]
     return 2.0*somatorio_dos_erros/len(erros_por_cordenada)
 
-def calcula_y(theta_0,theta_1,x):
+
+def calcula_y(theta_0, theta_1, x):
     return theta_0 + theta_1 * x
+
 
 def compute_mse(theta_0, theta_1, data):
     """
@@ -32,16 +37,21 @@ def compute_mse(theta_0, theta_1, data):
     """
     somatorio_dos_quadrados = 0
     for data_entry in data:
-        somatorio_dos_quadrados += pow(calcula_y(theta_0,theta_1,data_entry[0]) - data_entry[1],2)
+        somatorio_dos_quadrados += pow(calcula_y(theta_0,
+                                       theta_1, data_entry[0]) - data_entry[1], 2)
     return somatorio_dos_quadrados/len(data)
 
-def calcula_erros_por_cordenada(theta_0,theta_1,data):
+
+def calcula_erros_por_cordenada(theta_0, theta_1, data):
     valor_por_cordenada = []
     quantidade_de_dados = len(data)
     for i in range(quantidade_de_dados):
-        estimativa_calculada = calcula_y(theta_0,theta_1,data[i][COORDENADA_X]) - data[i][COORDENADA_Y]
-        valor_por_cordenada.append([data[i][COORDENADA_X],data[i][COORDENADA_Y],estimativa_calculada])
+        estimativa_calculada = calcula_y(
+            theta_0, theta_1, data[i][COORDENADA_X]) - data[i][COORDENADA_Y]
+        valor_por_cordenada.append(
+            [data[i][COORDENADA_X], data[i][COORDENADA_Y], estimativa_calculada])
     return valor_por_cordenada
+
 
 def step_gradient(theta_0, theta_1, data, alpha):
     """
@@ -52,26 +62,29 @@ def step_gradient(theta_0, theta_1, data, alpha):
     :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
     :return: float,float - os novos valores de theta_0 e theta_1, respectivamente
     """
-    erros_por_cordenada = calcula_erros_por_cordenada(theta_0,theta_1,data)
-    return theta_0 - alpha*deriva_theta_0(erros_por_cordenada),theta_1 - alpha*deriva_theta_1(erros_por_cordenada)
+    erros_por_cordenada = calcula_erros_por_cordenada(theta_0, theta_1, data)
+    return theta_0 - alpha*deriva_theta_0(erros_por_cordenada), theta_1 - alpha*deriva_theta_1(erros_por_cordenada)
+
 
 def normaliza_features(entry_data):
     min = Infinity
     max = - Infinity
-    
+
     for data in entry_data:
         min = data[COORDENADA_X] if data[COORDENADA_X] < min else min
         max = data[COORDENADA_X] if data[COORDENADA_X] > max else max
 
-    normaliza = lambda x:  (x-min) / (max-min)
+    def normaliza(x): return ((x-min)/(max-min))
+
     dados_normalizados = []
     for i in range(len(entry_data)):
-        dados_normalizados.append([normaliza(entry_data[i][COORDENADA_X]),entry_data[i][COORDENADA_Y]])
-        
-    
+        dados_normalizados.append(
+            [normaliza(entry_data[i][COORDENADA_X]), entry_data[i][COORDENADA_Y]])
+
     return dados_normalizados
 
-def fit(data, theta_0, theta_1, alpha, num_iterations):
+
+def fit(data, theta_0, theta_1, alpha, num_iterations, normalize_features=False):
     """
     Para cada época/iteração, executa uma atualização por descida de
     gradiente e registra os valores atualizados de theta_0 e theta_1.
@@ -90,13 +103,14 @@ def fit(data, theta_0, theta_1, alpha, num_iterations):
     lista_theta_1 = []
     theta_0_atual = theta_0
     theta_1_atual = theta_1
-    
-    data_normalizada = normaliza_features(data)
-    
+
+    data_normalizada = normaliza_features(data) if normalize_features else data
+
     for i in range(num_iterations):
-        novo_theta_0,novo_theta_1 = step_gradient(theta_0_atual, theta_1_atual, data_normalizada, alpha)
+        novo_theta_0, novo_theta_1 = step_gradient(
+            theta_0_atual, theta_1_atual, data_normalizada, alpha)
         lista_theta_0.append(novo_theta_0)
         lista_theta_1.append(novo_theta_1)
         theta_0_atual = novo_theta_0
         theta_1_atual = novo_theta_1
-    return lista_theta_0,lista_theta_1
+    return lista_theta_0, lista_theta_1
