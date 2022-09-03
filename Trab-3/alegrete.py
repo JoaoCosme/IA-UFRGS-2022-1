339503f8b9b718cffcf5cd04d392368f81a36cfc
JoaoCosme <49664603+JoaoCosme@users.noreply.github.com>
2022-09-03 14:19:45 -0300

    Normaliza features (#15)
    * adiciona normalizacao
    
    * adiciona normalizacao teste
    
    * ajusta np
    |
    
    * remove mudancas normalizacao
    
    * normaliza y tambem
    
    * adiciona imagens ao readme|
    
    * todos testes passando
    
    * Trigger

--- Trab-3/alegrete.py
+++ Trab-3/alegrete.py
@@ -1,9 +1,14 @@
+from logging import log
+import pprint as pp
 from time import thread_time
+
+from numpy import Infinity
 
 
 COORDENADA_X = 0
 COORDENADA_Y = 1
-VALOR_CALCULADO = 2 
+VALOR_CALCULADO = 2
+
 
 def deriva_theta_0(erros_por_cordenada):
     somatorio_dos_erros = 0
@@ -11,14 +16,18 @@ def deriva_theta_0(erros_por_cordenada):
         somatorio_dos_erros += erro_por_coordenada[VALOR_CALCULADO]
     return 2.0*somatorio_dos_erros/len(erros_por_cordenada)
 
+
 def deriva_theta_1(erros_por_cordenada):
     somatorio_dos_erros = 0
     for erro_por_coordenada in erros_por_cordenada:
-        somatorio_dos_erros += erro_por_coordenada[VALOR_CALCULADO] * erro_por_coordenada[COORDENADA_X]
+        somatorio_dos_erros += erro_por_coordenada[VALOR_CALCULADO] * \
+            erro_por_coordenada[COORDENADA_X]
     return 2.0*somatorio_dos_erros/len(erros_por_cordenada)
 
-def calcula_y(theta_0,theta_1,x):
-    return theta_0 + theta_1 * x
+
+def calcula_y(theta_0, theta_1, x):
+    return theta_0 + (theta_1 * x)
+
 
 def compute_mse(theta_0, theta_1, data):
     """
@@ -30,16 +39,21 @@ def compute_mse(theta_0, theta_1, data):
     """
     somatorio_dos_quadrados = 0
     for data_entry in data:
-        somatorio_dos_quadrados += pow(calcula_y(theta_0,theta_1,data_entry[0]) - data_entry[1],2)
+        somatorio_dos_quadrados += pow(calcula_y(theta_0,
+                                       theta_1, data_entry[0]) - data_entry[1], 2)
     return somatorio_dos_quadrados/len(data)
 
-def calcula_erros_por_cordenada(theta_0,theta_1,data):
+
+def calcula_erros_por_cordenada(theta_0, theta_1, data):
     valor_por_cordenada = []
     quantidade_de_dados = len(data)
     for i in range(quantidade_de_dados):
-        estimativa_calculada = calcula_y(theta_0,theta_1,data[i][COORDENADA_X]) - data[i][COORDENADA_Y]
-        valor_por_cordenada.append([data[i][COORDENADA_X],data[i][COORDENADA_Y],estimativa_calculada])
+        estimativa_calculada = calcula_y(
+            theta_0, theta_1, data[i][COORDENADA_X]) - data[i][COORDENADA_Y]
+        valor_por_cordenada.append(
+            [data[i][COORDENADA_X], data[i][COORDENADA_Y], estimativa_calculada])
     return valor_por_cordenada
+
 
 def step_gradient(theta_0, theta_1, data, alpha):
     """
@@ -50,10 +64,34 @@ def step_gradient(theta_0, theta_1, data, alpha):
     :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
     :return: float,float - os novos valores de theta_0 e theta_1, respectivamente
     """
-    erros_por_cordenada = calcula_erros_por_cordenada(theta_0,theta_1,data)
-    return theta_0 - alpha*deriva_theta_0(erros_por_cordenada),theta_1 - alpha*deriva_theta_1(erros_por_cordenada)
+    erros_por_cordenada = calcula_erros_por_cordenada(theta_0, theta_1, data)
+    return theta_0 - alpha*deriva_theta_0(erros_por_cordenada), theta_1 - alpha*deriva_theta_1(erros_por_cordenada)
 
-def fit(data, theta_0, theta_1, alpha, num_iterations):
+
+def normaliza_features(entry_data):
+    min_x = Infinity
+    max_x = - Infinity
+    min_y = Infinity
+    max_y = - Infinity
+
+    for data in entry_data:
+        min_x = data[COORDENADA_X] if data[COORDENADA_X] < min_x else min_x
+        max_x = data[COORDENADA_X] if data[COORDENADA_X] > max_x else max_x
+        min_y = data[COORDENADA_Y] if data[COORDENADA_Y] < min_y else min_y
+        max_y = data[COORDENADA_Y] if data[COORDENADA_Y] > max_y else max_y
+
+    def normaliza(x): return ((x-min_x)/(max_x-min_x))
+    def normaliza_y(x): return ((x-min_y)/(max_y-min_y))
+
+    dados_normalizados = []
+    for i in range(len(entry_data)):
+        dados_normalizados.append(
+            [normaliza(entry_data[i][COORDENADA_X]), normaliza_y(entry_data[i][COORDENADA_Y])])
+
+    return dados_normalizados
+
+
+def fit(data, theta_0, theta_1, alpha, num_iterations, normalize_features=False):
     """
     Para cada época/iteração, executa uma atualização por descida de
     gradiente e registra os valores atualizados de theta_0 e theta_1.
@@ -72,10 +110,14 @@ def fit(data, theta_0, theta_1, alpha, num_iterations):
     lista_theta_1 = []
     theta_0_atual = theta_0
     theta_1_atual = theta_1
+
+    data_normalizada = normaliza_features(data) if normalize_features else data
+
     for i in range(num_iterations):
-        novo_theta_0,novo_theta_1 = step_gradient(theta_0_atual, theta_1_atual, data, alpha)
+        novo_theta_0, novo_theta_1 = step_gradient(
+            theta_0_atual, theta_1_atual, data_normalizada, alpha)
         lista_theta_0.append(novo_theta_0)
         lista_theta_1.append(novo_theta_1)
         theta_0_atual = novo_theta_0
         theta_1_atual = novo_theta_1
-    return lista_theta_0,lista_theta_1
+    return lista_theta_0, lista_theta_1
--- Trab-3/readme.md
+++ Trab-3/readme.md
@@ -4,7 +4,7 @@
 
 |Nome|Cartao|Turma|
 |---|---|----|
-|Alice Santin Varella| 00259915 | B 
+|Alice Santin Varella| 00259915 | B
 |Joao Pedro Cosme da Silva|00314792 |A
 |Eduardo André Leite | 00287684| A
 
@@ -18,19 +18,64 @@ Um do conjuntos de valores que consegue atingir a execução bem sucedida (0 Ata
 
 -> k:int (Número de participantes do torneio) = 2
 
--> m:float (Probabilidade de mutação (entre 0 e 1)) = 0.5 
+-> m:float (Probabilidade de mutação (entre 0 e 1)) = 0.5
 
--> e:bool (Verdadeiro se há elitismo) = True 
+-> e:bool (Verdadeiro se há elitismo) = True
 
 Menor número de ataques = 0.
 
+![graf](Graph_T3_IA.png)
+
 ## Regressão Linear
 
-Melhor run:
+Foi utilizado o algoritmo de regressão linear por reta, usando MSE como medida de erro. Theta 0 e Theta 1 são randomicamente inicializados com valores reais entre 0 e 1.
 
-```python
-theta_0s, theta_1s = alegrete.fit(
-    quiz_data, theta_0=random.randint(-100,100), theta_1=random.randint(-100,100), 
-    alpha=0.01, num_iterations=1000
-)
-```
+### Normalização de Features
+
+Foi adicionado um novo parametro a função de `fit`, um booleano para ativar a normalização de features. Neste caso, as features são normalizadas entre 0 e 1, usando o algortimo visto em aula, que possui a forma:
+
+$(x-min_x/max_x-max_x)$
+
+### Testes
+
+A seguir, exemplificamos os melhores resultados de cada caso. Como o dataset é composto por apenas uma informação de entrada e uma de saida, podemos ver que a normalização de features neste caso não exerce um impacto significativo na perfomance do algoritmo, já que não há variaveis de grandezas diferentes em ação.
+
+#### Sem normalização de features
+
+|Variavel|Valor|
+|-|-
+|Theta0|Random
+|Theta1|Random
+|Alfa|0.01
+|Iteracoes até estabilizar|<1000
+EQM final| 8.52770819098256
+
+##### Resultados
+
+###### Erro Quadratico Medio
+
+![erro_medio](erro_medio_sem_norm.png)
+
+###### Reta Resultante
+
+![reta](reta_sem_norm.png)
+
+#### Com Normalização de Features
+
+|Variavel|Valor|
+|-|-
+|Theta0|Random
+|Theta1|Random
+|Alfa|0.01
+|Iteracoes até establizar|<3000
+EQM final| 10.820954179569718
+
+
+##### Resultados
+
+###### Erro Quadratico Medio
+![erro_medio](erro_medio_com_norm.png)
+
+
+###### Reta Resultante
+![reta](reta_com_norm.png)
